1d2a62ba5091e555ba6ab23586cb9677379b441d
Clara <168085353+indomiekwah@users.noreply.github.com>
2026-02-16 02:45:52 +0700

    Update login-script.js
    Mengganti komentar sesuai dengan format JSDoc

--- assets/scripts/login-script.js
+++ assets/scripts/login-script.js
@@ -1,23 +1,51 @@
-/*
- * Catatan:
- * Gunakan fungsi goToHome() untuk menampilkan halaman home
- * Gunakan fungsi goToLogin() untuk menampilkan halaman login
- * Gunakan fungsi showPopUp() untuk menampilkan pop up error
- */
-
+/** @module login-scripts.js */
+/**
+* Membuat variabel loginFormElement untuk tampilan form.
+* @constant {HTMLElement}
+*/
 const loginFormElement = document.querySelector('#loginForm');
+/**
+* Membuat variabel inputEmailElement untuk tampilan input email.
+* @constant {HTMLElement}
+*/
 const inputEmailElement = document.querySelector('#inputEmail');
+/**
+* Membuat variabel inputPasswordElement untuk tampilan input password.
+* @constant {HTMLElement}
+*/
 const inputPasswordElement = document.querySelector('#inputPassword');
 
+
+/**
+* Membuat variabel expected email untuk menyimpan informasi email sementara
+* @constant {string}
+*/
 const expectedEmail = 'admin@dicoding.com';
+/**
+* Membuat variabel expected email untuk menyimpan informasi password sementara
+* @constant {string}
+*/
 const expectedPassword = 'superpassword';
 
+/* Comment: Menambah aksi klik pada button. */
 loginFormElement.addEventListener('submit', function(event) {
   event.preventDefault();
 
+  /**
+  * Membuat variabel email untuk menyimpan nilai email yang didapatkan saat button ditekan.
+  * @constant {string}
+  */
   const email = inputEmailElement.value;
+  /**
+  * Membuat variabel email untuk menyimpan nilai password yang didapatkan saat button ditekan.
+  * @constant {string}
+  */
   const password = inputPasswordElement.value;
+  
+  /* Comment: Memastikan bahwa nilai email dan password sama dengan nilai yang tersimpan */
   if (email == expectedEmail && password == expectedPassword) {
+    
+    /* Comment: Jika sesuai, maka program akan berpindah ke halaman utama */
     goToHome();
   } else {
     showPopUp();
